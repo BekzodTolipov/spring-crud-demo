6fdd390f595b727d412475dd7ecd51e4260ac5f3
Bek <btolipov110@gmail.com>
2020-08-02 16:48:01 -0500

    Thymeleaf implementation demo to display time on server

--- src/main/java/com/bek/spring/crud/demo/employeecruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/bek/spring/crud/demo/employeecruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,8 @@ package com.bek.spring.crud.demo.employeecruddemo.dao;
 
 import com.bek.spring.crud.demo.employeecruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members") // Custom defined path name
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-
-
 }
